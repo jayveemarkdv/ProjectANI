a9d98fc7b5ed15e71585d919ed465fbcf1064d28
jayvzee <30379184+jayvzee@users.noreply.github.com>
2021-06-17 09:46:25 +0800

    nav bar (mej tapos)

--- app/src/main/java/com/example/projectani/DashboardActivity.kt
+++ app/src/main/java/com/example/projectani/DashboardActivity.kt
@@ -1,20 +1,43 @@
 package com.example.projectani
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.drawerlayout.widget.DrawerLayout
 import java.text.SimpleDateFormat
 
 class DashboardActivity : AppCompatActivity(){
 
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-        val tvDisplayDate = findViewById<TextView>(R.id.dateText)
-        val date = System.currentTimeMillis()
+        setContentView(R.layout.activity_main)
 
-        val sdf = SimpleDateFormat("MMM MM dd, yyyy h:mm a")
-        val dateString: String = sdf.format(date)
-        tvDisplayDate.text = dateString
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miItem1 -> Toast.makeText(applicationContext, "clicked item 1", Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext, "clicked item 2", Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext, "clicked item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
